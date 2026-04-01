8db833fcf27e4f894afa0261c9f06bcc6ce72c8f
rwr1Mahipal <rwr1.mahipal.sb@gmail.com>
2026-04-01 15:49:26 +0530

    Add files via upload

---
+++ DSA/tree.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+class BinaryTree{
+    public:
+    Node* root;
+    BinaryTree(){
+        root=NULL;
+    }
+
+    Node* createTree(){
+        int val;
+        cin>>val;
+
+        if(val == -1) return NULL;
+        Node* node = new Node(val);
+        node->left=createTree();
+        node->right=createTree();
+        return node;
+    }
+
+
+    void preOrder(Node* node){
+        if(node==NULL) return;
+        cout<<node->data<<" ";
+        preOrder(node->left);
+        preOrder(node->right);
+    }
+
+    void inOrder(Node* node){
+        if(node==NULL) return;
+        inOrder(node->left);
+        cout<<node->data<<" ";
+        inOrder(node->right);
+    }
+
+    void postOrder(Node* node){
+        if(node==NULL) return;
+        postOrder(node->left);
+        postOrder(node->right);
+        cout<<node->data<<" ";
+    }
+};
+
+int main(){
+    BinaryTree b;
+
+    // b.root = new Node(1);
+    // b.root->left = new Node(2);
+    // b.root->right = new Node(3);
+    // b.root->left->left = new Node(4);
+    // b.root->left->right = new Node(5);
+
+    cout<<"Enter Tree value (use -1 for NULL) ";
+    b.root = b.createTree();
+
+    cout<<"PreOrder: ";
+    b.preOrder(b.root);
+
+    cout<<"\nInOrder: ";
+    b.inOrder(b.root);
+
+    cout<<"\nPostOrder: ";
+    b.postOrder(b.root);
+}
