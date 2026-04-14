a781deb2c42244eab75be2c395ebede36c452d38
rwr1Mahipal <rwr1.mahipal.sb@gmail.com>
2026-04-14 15:51:56 +0530

    Add files via upload

---
+++ JS/1.js
@@ -0,0 +1,21 @@
+// var a: number = 10;
+// console.log(a);
+// var a: number = 20;
+// console.log(a);
+// let b = 100;
+// b = 1000;
+// console.log(b);
+// const c = 200;
+
+var a = 10;
+var str = "javaScript";
+var user = false;
+var b = null;
+var c = undefined;
+var maxNum = BigInt(25478);
+var isUser = ["user", "admin", "manager"];
+console.log(str);
+
+document.write(str)
+
+document.getElementById("text").innerHTML = str;
---
+++ JS/1.ts
@@ -0,0 +1,18 @@
+// var a: number = 10;
+// console.log(a);
+// var a: number = 20;
+// console.log(a);
+
+// let b = 100;
+// b = 1000;
+// console.log(b);
+
+// const c = 200;
+
+var a: number = 10;
+var str: string = "javaScript";
+var user: boolean = false;
+var b = null;
+var c = undefined;
+var maxNum: bigint = BigInt(25478);
+var isUser = ["user", "admin", "manager"];
