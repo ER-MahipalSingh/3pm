7c51990918dc5c4314405123fb46ee4936ae1068
MAHIPAL SINGH <rwr1.mahipal.sb@gmail.com>
2026-05-20 16:16:09 +0530

    Add files via upload

---
+++ JS/api.js
@@ -0,0 +1,32 @@
+const fetchAPI = async () => {
+  try {
+    const res = await fetch("https://dummyjson.com/products");
+    if (!res.ok) {
+      throw new Error("Api CANT FETCH");
+    }
+    const data = await res?.json();
+    // console.log(data.products);
+    renderData(data?.products);
+    return data;
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+};
+fetchAPI();
+
+const renderData = (products) => {
+  console.log(products);
+
+  const container = document.getElementById("container");
+  container.innerHTML = "";
+
+  products?.forEach((product) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <h1>${product.title}</h1>
+    <img src=${product.thumbnail}>
+    `;
+    container.appendChild(div);
+  });
+};
+renderData();
